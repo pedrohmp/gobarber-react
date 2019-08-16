942a1c1a8db204ff2a6883b24b388b131e22f9e7
Pedro <pedrohmp@icloud.com>
2019-08-16 01:17:08 -0300

    first commit

---
+++ src/pages/SignUp/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+
+import logo from '~/assets/logo.svg'
+
+export default function SignUp() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+  return (
+    <>
+    <img src={logo} alt="Gobarber"/>
+
+    <Form onSubmit={handleSubmit}>
+      <Input name="name" type="text" placeholder="Nome completo" />
+      <Input name="email" type="email" placeholder="Seu e-mail" />
+      <Input name="password" type="password" placeholder="Sua senha secreta" />
+
+      <button type="submit">Criar conta</button>
+      <Link to="/">Ja tenho login</Link>
+    </Form>
+    </>
+  );
+}
